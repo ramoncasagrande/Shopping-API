e1895a9ed3732ce9b7ca0124eb30f0524babd97d
Ramon Casagrande <ramoncasagrande@hotmail.com>
2022-11-28 15:42:51 -0300

    Criado método para inserir usuário

--- src/main/java/com/shopping/api/Controller/UserController.java
+++ src/main/java/com/shopping/api/Controller/UserController.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shopping.api.Model.UserModel;
@@ -60,5 +62,11 @@ public class UserController {
         }
         return null;
     }
-    
+
+    @PostMapping("/newUser")
+    public UserModel insertUser(@RequestBody UserModel userModel){
+        userModel.setDataCadastro(new Date());
+        userList.add(userModel);
+        return userModel;
+    }
 }
