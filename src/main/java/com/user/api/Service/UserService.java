262d1722ddbde5efd7c440c7477637ebac2d6bc0
Ramon Casagrande <ramoncasagrande@hotmail.com>
2022-11-29 10:19:28 -0300

    Criados mais métodos de Serviço

--- src/main/java/com/user/api/Service/UserService.java
+++ src/main/java/com/user/api/Service/UserService.java
@@ -1,6 +1,7 @@
 package com.user.api.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,43 @@ public class UserService {
 
     public List<UserDTO> getAll(){
         List<User> users = userRepository.findAll();
+        return users
+                .stream()
+                .map(UserDTO::convert)
+                .collect(Collectors.toList());
+    }
+
+    public UserDTO findById(Long userId){
+        Optional<User> user = userRepository.findById(userId);
+        if(user.isPresent()){
+            return UserDTO.convert(user.get());
+        }
+        return null;
+    }
+
+    public UserDTO save(UserDTO userDTO){
+        User user = userRepository.save(User.convert(userDTO));
+        return UserDTO.convert(user);
+    }
+
+    public UserDTO delete(Long userId){
+        Optional<User> user = userRepository.findById(userId);
+        if(user.isPresent()){
+            userRepository.delete(user.get());
+        }
+        return null;
+    }
+
+    public UserDTO findByCpf(String userCpf){
+        User user = userRepository.findByCpf(userCpf);
+        if(user != null){
+            return UserDTO.convert(user);
+        }
+        return null;
+    }
+
+    public List<UserDTO> queryByName(String nome){
+        List<User> users = userRepository.queryByNomeLike(nome);
         return users
                 .stream()
                 .map(UserDTO::convert)
