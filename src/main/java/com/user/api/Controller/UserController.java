b56f2626ce6864ade1f4931e0e32ee3f11de5451
Ramon Casagrande <ramoncasagrande@hotmail.com>
2022-11-29 11:39:56 -0300

    Correção da anotação AutoWired no Controller

--- src/main/java/com/user/api/Controller/UserController.java
+++ src/main/java/com/user/api/Controller/UserController.java
@@ -2,6 +2,7 @@ package com.user.api.Controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.user.api.Service.UserService;
 @RestController
 public class UserController {
 
+    @Autowired
     private UserService userService;
 
     @GetMapping("/user/")
